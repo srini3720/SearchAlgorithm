80a08d9ddd218134ccff01547eaa345b8dae42d2
srinivasan <srini3720@gmail.com>
2022-07-25 16:04:18 +0530

    golang mysql setup

---
+++ go.mod
@@ -0,0 +1,9 @@
+module poc/search
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/spf13/viper v1.12.0 // indirect
+)
---
+++ search.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+type question struct {
+	ID int
+	question_name string
+	category_id int
+	topic_id int
+	subject_id int
+	subcategory_id int
+	
+}
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+	  log.Fatalf("Error loading .env file")
+	}
+	dbHost := os.Getenv("DB_HOST")
+	fmt.Println("DB String: ", dbHost)
+
+
+	
+	db, err := sql.Open("mysql", dbHost)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Connected to MySQL")
+
+	var questionsuggestion = getAllQuestion(db);
+	fmt.Println("questionsuggestion",questionsuggestion)
+
+
+
+	defer db.Close()
+}
+
+func getAllQuestion(db *sql.DB) (suggestion []question) {
+		// select
+		results, err := db.Query("SELECT ID,question_name,category_id,topic_id,subject_id,subcategory_id FROM esdb.question limit 100")
+		// defer results.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+
+		
+		for results.Next() {
+			var Question question
+			err = results.Scan(&Question.ID, &Question.question_name, &Question.category_id,&Question.topic_id,&Question.subject_id,&Question.subcategory_id)
+			if err != nil {
+				panic(err.Error())
+			}
+			fmt.Println("--------------Question---------------",Question.ID,Question.question_name,Question.category_id,Question.topic_id,Question.subject_id,Question.subcategory_id)
+			suggestion = append(suggestion, Question)
+		}
+		return suggestion
+	
+}
+
+
